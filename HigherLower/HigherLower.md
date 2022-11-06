1c443262e36f7fa36b4557675b7d50ea54d3f38f
polo871209 <qazh0123@gmail.com>
2022-11-06 18:40:01 +0800

    optimize functions

---
+++ HigherLower/HigherLower.md
@@ -0,0 +1,8 @@
+# HigherLower game
+
+Inspire by **[higherlower gamee](http://www.higherlowergame.com/ "higherlowergame.com")**  
+
+** Comparing which instagram brand/celebrity had more followers **
+
+## Results
+
--- HigherLower/main.py
+++ HigherLower/main.py
@@ -10,16 +10,11 @@ df = pd.read_csv(file, encoding='latin-1')
 rank_brand = pd.DataFrame(df, columns=['RANK', 'BRAND', 'FOLLOWERS'])
 
 
-def search_rank(name):
-    """Search rank from name"""
+def rank_follower(name):
+    """Search rank and follower from name"""
     res_df = rank_brand[rank_brand['BRAND'].str.startswith(name)]
-    return res_df.values.tolist()[0][0]
-
-
-def search_follower(name):
-    """Search follower from name"""
-    res_df = rank_brand[rank_brand['BRAND'].str.startswith(name)]
-    return res_df.values.tolist()[0][2][:5]
+    lst = res_df.values.tolist()
+    return lst[0][0], lst[0][2][:5]
 
 
 def random_brand(datafram=rank_brand):
@@ -27,26 +22,27 @@ def random_brand(datafram=rank_brand):
     return datafram.sample().values.tolist()[0][1]
 
 
-score = 0
-while True:
-    brand_1, brand_2 = random_brand(), random_brand()  # Generate guessing brand
-    if brand_1 == brand_2:
-        brand_2 = random_brand()
-    brand_1_rank, brand_2_rank = search_rank(brand_1), search_rank(brand_2)
-    brand_1_follower, brand_2_follower = search_follower(
-        brand_1), search_follower(brand_2)
-    if brand_1_rank > brand_2_rank:
-        answer = 'Y'
-    else:
-        answer = 'N'
+if __name__ == '__main__':
+    score = 0
+    while True:
+        brand_1, brand_2 = random_brand(), random_brand()  # Generate guessing brand
+        if brand_1 == brand_2:
+            brand_2 = random_brand()
+        b1_rank, b1_follower = rank_follower(brand_1)
+        b2_rank, b2_follower = rank_follower(brand_2)
+        if b1_rank < b2_rank:
+            answer = 'Y'
+        else:
+            answer = 'N'
+        print(f'\'{brand_1}\' has more follower than \'{brand_2}\'')
+        guess = input('Y or N: ')
 
-    print(f'\'{brand_1}\' has more follower than \'{brand_2}\'')
-    guess = input('Y or N: ')
-
-    if answer in guess.upper():
-        score += 1
-        print(f'Good job! {brand_1}: {brand_1_follower},  {brand_2}: {brand_2_follower}\nScore:{score}')
-        continue
-    else:
-        print(f'Failed! {brand_1}: {brand_1_follower},  {brand_2}: {brand_2_follower}\nScore:{score}')
-        break
+        if answer in guess.upper():
+            score += 1
+            print(
+                f'Good job! {brand_1}: {b1_follower},  {brand_2}: {b2_follower}\nScore:{score}')
+            continue
+        else:
+            print(
+                f'Failed! {brand_1}: {b1_follower},  {brand_2}: {b2_follower}\nScore:{score}')
+            break
